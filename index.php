b820f46eb15c5992a16a56f9c7f4d40897ef5045
raphael anderson <raphael.8379@hotmail.com>
2020-11-23 09:30:17 -0300

    arquivo index.php

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" href="style.css" />
+<link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+<link rel="manifest" href="favicon/site.webmanifest">
+<link rel="mask-icon" href="favicon/safari-pinned-tab.svg" color="#5bbad5">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="theme-color" content="#ffffff">
+<title>Raphael Anderson RA</title>
+</head>
+
+<body>
+				  
+				<input type="checkbox" id="check" />
+                <label id="icone" for="check"> <img src="icone.png" /> </label>
+                
+               
+                	
+                <div class="barra">
+                
+                <nav>
+                <a href=""> <div class="link">  Home </div> </a>
+                <a href=""> <div class="link">  Tutoriais </div> </a>
+                <a href=""> <div class="link">  Download </div> </a>
+                <a href=""> <div class="link">  Eventos </div> </a>
+                <a href=""> <div class="link">  Contato </div> </a>
+                
+                </nav>
+                
+                </div>	
+                
+               
+                    
+</body>
+</html>
